76406b92a6ecd5e0dbc5392c6921549b31e02d97
Desktop <Desktop@Desktop-PC>
2019-09-08 20:14:21 +0530

    count char with scanner

--- String/src/com/nt/string/CountCharString.java
+++ String/src/com/nt/string/CountCharString.java
@@ -1,10 +1,15 @@
 package com.nt.string;
 
+import java.util.Scanner;
+
 public class CountCharString {
 
 	public static void main(String[] args) {
 		
-		String s="vipin kumar sahu";
+	/*	String s="vipin kumar sahu";*/
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter String::");
+		String s=sc.nextLine();
 		int count=0;
 		for(int i=0;i<s.length();i++) {
 			if(s.charAt(i)!=' ') {
